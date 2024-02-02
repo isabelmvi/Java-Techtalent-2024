1530a5f16cb5046be66dd563ddceabbfd49809af
isabelmvi <158471728+isabelmvi@users.noreply.github.com>
2024-02-02 12:36:43 +0100

    Update PruebaApp.java

--- Java_1st_project/src/PruebaApp.java
+++ Java_1st_project/src/PruebaApp.java
@@ -4,7 +4,7 @@ public class PruebaApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=30;
-		int b=305;
+		double b=305;
 		double suma=a+b; //Si fuera un int daría error
 		System.out.print(suma);
 		
