51bc1c2b9189facfea865ff964c8cae2ef0534e0
isabelmvi <isabel.murariu@gmail.com>
2024-02-02 12:28:29 +0100

    Create PruebaApp.java

---
+++ Java_1st_project/src/PruebaApp.java
@@ -0,0 +1,13 @@
+
+public class PruebaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=305;
+		double suma=a+b; //Si fuera un int daría error
+		System.out.print(suma);
+		
+	}
+
+}
