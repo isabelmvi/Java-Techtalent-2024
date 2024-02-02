ea841d4d5525b3d85f9371f03e882183ae3229ab
isabelmvi <isabel.murariu@gmail.com>
2024-02-02 13:10:05 +0100

    PruebaApp2-3

---
+++ Java_1st_project/src/PruebaApp3.java
@@ -0,0 +1,11 @@
+
+public class PruebaApp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30, b=60, c=20;
+		double suma=a+b+c; //Si fuera un int daría error
+		System.out.print(suma);
+	}
+
+}
---
+++ Java_1st_project/src/PruebaApp4.java
@@ -0,0 +1,14 @@
+
+public class PruebaApp4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double IVA = 0.18;
+		int producto=300;
+		
+		double resultado=producto*IVA;
+		
+		System.out.println("El IVA del producto es"+ resultado);
+	}
+
+}
