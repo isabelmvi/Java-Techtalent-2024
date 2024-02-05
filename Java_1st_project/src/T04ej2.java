8635c73f6d7bcc83a239976976b486e333aab3b4
isabelmvi <isabel.murariu@gmail.com>
2024-02-05 13:00:17 +0100

    Create T04ej2.java

---
+++ Java_1st_project/src/T04ej2.java
@@ -0,0 +1,26 @@
+
+public class T04ej2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N=5;
+		double A=8.5;
+		char C='a';
+		int a=26;
+		
+		double suma=0;
+		double resta=0;
+		
+		suma=(N+A);
+		resta=(A-N);
+		
+		System.out.println("Variable N = "+N);
+		System.out.println("Variable A = "+A);
+		System.out.println("Variable C = "+C);
+		System.out.println(+N+ " + " +A+ " = " + suma);
+		System.out.println(+N+ " - " +A+ " = " + resta);
+		System.out.println("Valor numérico del carácter a = " +a);
+		
+	}
+
+}
