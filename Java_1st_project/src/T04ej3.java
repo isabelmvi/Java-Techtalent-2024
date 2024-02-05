bb3b96fe5a7eda494b5fa24bef0bb8104ea39cd8
isabelmvi <isabel.murariu@gmail.com>
2024-02-05 13:38:06 +0100

    Create T04ej3.java

---
+++ Java_1st_project/src/T04ej3.java
@@ -0,0 +1,32 @@
+
+public class T04ej3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int X=2;
+		int Y=6;
+		double N=8.6575;
+		double M=3.14156;
+		
+		System.out.println("X+Y="+(X+Y));
+		System.out.println("X-Y="+(X-Y));
+		System.out.println("X*Y="+(X*Y));
+		System.out.println("X/Y="+(X/Y));
+		System.out.println("X%Y="+(X%Y));
+		System.out.println("N+M="+(N+M));
+		System.out.println("N-M="+(N-M));
+		System.out.println("N*M="+(N*M));
+		System.out.println("N/M="+(N/M));
+		System.out.println("N%M="+(N%M));
+		System.out.println("X+N="+(X+N));
+		System.out.println("Y/M="+(Y/M));
+		System.out.println("Y%M="+(Y%M));
+		System.out.println("2X*="+2*X);
+		System.out.println("2Y="+2*Y);
+		System.out.println("2N="+2*N);
+		System.out.println("2M="+2*M);
+		System.out.println("X+Y+N+M="+(X+Y+N+M));
+		System.out.println("X*Y*N*M="+(X*Y*N*M));
+	}
+
+}
