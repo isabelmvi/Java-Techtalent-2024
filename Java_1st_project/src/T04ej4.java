85d0d12c72a0d1a7a2a0a9eef03dfa265c9478e7
isabelmvi <isabel.murariu@gmail.com>
2024-02-05 13:47:32 +0100

    Create T04ej4.java

---
+++ Java_1st_project/src/T04ej4.java
@@ -0,0 +1,15 @@
+
+public class T04ej4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N=4;
+		
+		System.out.println("N+77="+(N+77));
+		System.out.println("N-3="+(N-3));
+		System.out.println("N*2="+(2*N));
+		
+		
+	}
+
+}
