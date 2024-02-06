4e94378355ba35eb4707f679abe7b80c2e930a47
isabelmvi <isabel.murariu@gmail.com>
2024-02-06 19:22:39 +0100

    Create T05ej3.java

---
+++ Java_1st_project/src/T05ej3.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class T05ej3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre=JOptionPane.showInputDialog("Introduce tu nombre");
+		JOptionPane.showMessageDialog(null, "Bienvenido/a "+nombre+"!");	
+	}
+
+}
