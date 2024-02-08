cabe6873f42eb0a7d39b9e6aa3bf1542e2ce1c13
isabelmvi <isabel.murariu@gmail.com>
2024-02-08 10:15:50 +0100

    Create T05ej11.java

---
+++ Java_1st_project/src/T05ej11.java
@@ -0,0 +1,36 @@
+
+public class T05ej11 {
+
+	public static void main(String[] args) {
+		String dia="Martes";
+		String laborable="Hoy es día laborable.";
+		String error="No has introducido un día correcto.";
+		switch(dia) {
+			case "Lunes":
+				System.out.println(laborable);
+				break;
+			case "Martes":
+				System.out.println(laborable);
+				break;
+			case "Miércoles":
+				System.out.println(laborable);
+				break;
+			case "Jueves":
+				System.out.println(laborable);
+				break;
+			case "Viernes":
+				System.out.println(laborable);
+				break;
+			case "Sábado":
+				System.out.println(laborable);
+				break;
+			case "Domingo":
+				System.out.println(laborable);
+				break;
+			default:
+				System.out.println(error);
+		}
+
+	}
+
+}
