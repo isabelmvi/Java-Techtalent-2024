a1f124672b8599c2e1983cecc91a4bfbe742613e
isabelmvi <isabel.murariu@gmail.com>
2024-02-13 11:40:52 +0100

    Create T05ejExtra3.java

---
+++ Java_1st_project/src/T05ejExtra3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class T05ejExtra3 {
+
+	public static void main(String[] args) {
+		
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Introdu un número: ");
+				int numero = scanner.nextInt();
+				scanner.close();
+				
+				int suma=0;
+		         for (int i = numero; i >= 1; i--) {
+		                suma += i;
+		     } 
+		         System.out.println("La suma de los números anteriores al " + numero + " es: " + suma);
+		}
+}
