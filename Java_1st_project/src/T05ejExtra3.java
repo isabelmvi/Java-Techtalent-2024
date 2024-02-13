84e580af92e736730d2a8e4e0dc430b6e08e85ae
isabelmvi <isabel.murariu@gmail.com>
2024-02-13 11:44:09 +0100

    Update T05ejExtra3.java

--- Java_1st_project/src/T05ejExtra3.java
+++ Java_1st_project/src/T05ejExtra3.java
@@ -4,7 +4,7 @@ public class T05ejExtra3 {
 	public static void main(String[] args) {
 		
 			Scanner scanner = new Scanner(System.in);
-			System.out.print("Introdu un número: ");
+			System.out.print("Introduce un número: ");
 				int numero = scanner.nextInt();
 				scanner.close();
 				
