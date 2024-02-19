574f7bed014d6010b482c13271f83aff815e6557
isabelmvi <isabel.murariu@gmail.com>
2024-02-19 13:26:10 +0100

    Create T06ej6.java

---
+++ Java_1st_project/src/UD6JavaBasics/T06ej6.java
@@ -0,0 +1,17 @@
+package UD6JavaBasics;
+import javax.swing.JOptionPane;
+public class T06ej6 {
+
+	public static void main(String[] args) {
+		
+		String numEnt=JOptionPane.showInputDialog("Introduce un número entero positivo: ");
+		int result=contar(numEnt);
+		JOptionPane.showMessageDialog(null, result);
+	
+	}
+	public static int contar(String numEnt) {
+		int length=numEnt.length();
+		return length;
+		
+	}
+}
